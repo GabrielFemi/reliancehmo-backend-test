4e3c50fc9f070dd7baa9ea4513385b8149482f42
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2021-01-17 06:13:22 +0100

    Use fallback for unhandled routes

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::fallback(function (){
+    return response()->json(['message' => 'Resource Not Found.'], 404);
+});
