ee36c5c1b04ee07c822d4802bff7b91964adb717
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2021-01-29 09:17:44 +0100

    wip

---
+++ database/factories/GameFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Game;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Arr;
+
+class GameFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Game::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        $namesOfgames = ['Call of Duty', 'Mortal Kombat', 'FIFA', 'Just Cause', 'Apex Legend'];
+        $game = Arr::random($namesOfgames);
+        return [
+            'name' => $game,
+            'version' => pickVersion($game),
+        ];
+    }
+}
